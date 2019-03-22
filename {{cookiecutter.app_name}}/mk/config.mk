2a301717c88ac3d286a57244308e0b33afa0e85f
Vivus Ignis <ignis@posteo.de>
2019-03-22 21:41:04 +0100

    lost PREPROCESSOR var is back

--- {{cookiecutter.app_name}}/mk/config.mk
+++ {{cookiecutter.app_name}}/mk/config.mk
@@ -11,5 +11,7 @@ GIT_REPO        := $(shell git config --get remote.origin.url)
 GIT_BRANCH      := $(shell git rev-parse --abbrev-ref HEAD)
 GIT_HASH        := $(shell git log -n 1 --pretty=format:'%h')
 
+PREPROCESSOR := gcc -E -x c -P -traditional-cpp
+
 export LC_ALL    = C.UTF-8
 export LANG      = C.UTF-8
